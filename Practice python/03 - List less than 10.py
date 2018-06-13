639c0a16130a728ea92f198156f9eff1eb0c220e
Narendhran Chidambaram <nchidambaram@corp.logitech.com>
2018-06-13 10:04:29 +0530

    20180613

--- Practice python/03 - List less than 10.py
+++ Practice python/03 - List less than 10.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 
@@ -14,20 +13,3 @@ for element in a:
   if element<10:
       b.append(element)
   print(b)
-=======
-
-
-
-
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-
-b=[]
-print(a)
-
-
-for element in a:
-  print(element)
-  if element<10:
-      b.append(element)
-  print(b)
->>>>>>> 7c37dfb9244c475e0f5ba101811c631df9a772b6
--- Test/pythonTest.py
+++ Test/pythonTest.py
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-from langdetect import detect
-print(detect("birds are colourful"))
-=======
-print("Hello")
->>>>>>> e098262d7bc0c5023aab1e18d9bedf7f221006de
+import re
+A='adbjhsabdshj.vjdb!@#$%$'
+B = re.sub('[^A-Za-z0-9]+', '', A)
+print(A,end='\n')
+print(B,end='\n')
