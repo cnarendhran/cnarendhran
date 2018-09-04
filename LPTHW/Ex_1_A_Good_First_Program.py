9a027b3a68f57043e7f48d10b83ab09e3184f568
Narendhran Chidambaram <nchidambaram@corp.logitech.com>
2018-09-04 17:30:17 +0530

    20180904

--- LPTHW/Ex_1_A_Good_First_Program.py
+++ LPTHW/Ex_1_A_Good_First_Program.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 print("Hello World!")
 print("Hello Again")
 print("I like typing this.")
@@ -10,13 +9,3 @@ print("""Hell'o """)
 
 
 
-=======
-print("Hello World!")
-print("Hello Again")
-print("I like typing this.")
-print("This is fun.")
-print('Yay! Printing.')
-print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
-print("""Hell'o """)
->>>>>>> e098262d7bc0c5023aab1e18d9bedf7f221006de
