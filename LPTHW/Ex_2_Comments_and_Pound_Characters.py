42661e64c8152a1d61d2339532b4b90e586488bf
Narendhran Chidambaram <nchidambaram@corp.logitech.com>
2018-09-04 18:30:38 +0530

    20180904

--- LPTHW/Ex_2_Comments_and_Pound_Characters.py
+++ LPTHW/Ex_2_Comments_and_Pound_Characters.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # A comment, this is so you can read your program later.
 # Anything after the # is ignored by python.
 
@@ -8,14 +7,3 @@ print("I could have code like this.")  # and the comment after is ignored
 # print("This won't run.")
 
 print("This will run.")
-=======
-# A comment, this is so you can read your program later.
-# Anything after the # is ignored by python.
-
-print("I could have code like this.")  # and the comment after is ignored
-
-# You can also use a comment to "disable" or comment out code:
-# print("This won't run.")
-
-print("This will run.")
->>>>>>> e098262d7bc0c5023aab1e18d9bedf7f221006de
